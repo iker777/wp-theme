a153607f5483bde2d296439f88c8ab9040bcad25
iker777 <ikeralvarez@gmail.com>
2025-05-07 16:58:01 +0200

    Watch added. Now we can run the command: npm run watch

--- front-page.php
+++ front-page.php
@@ -1,3 +1,6 @@
 <?php get_header(); ?>
-<h1 class="text-3xl font-bold text-blue-600">Hola mundo</h1>
+<h1 class="text-3xl font-bold text-orange-200">Hola mundo</h1>
+<h1 class="text-3xl font-bold text-orange-200">Hola mundo</h1>
+<h1 class="text-3xl font-bold text-orange-400">Hola mundo</h1>
+<h1 class="text-3xl font-bold text-red-400">Hola mundo</h1>
 <?php get_footer() ?>
